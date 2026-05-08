81b06a20807b16b565f4509213c6230c7d30124c
Pavithra Sunilkumar <pavithrasunilk68@gmail.com>
2026-05-09 01:50:41 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://pathaegis-backend.onrender.com',
+  baseURL: 'https://pathaegis.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
